c4f77f746410b72279e7d618ff7d6ed3b3a791c8
jairorudascto <jairo.rudas.cto@cartagenatripmap.com>
2024-04-28 21:55:50 -0300

    test: new test for card component

---
+++ products/src/components/molecules/card/test.cy.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { mount } from '@cypress/react18';
+import { Card } from './index';
+import './styles.module.css';
+
+describe('Card Component', () => {
+    const itemData = {
+        thumbnail: 'https://dummyjson.com/image/200x100',
+        title: 'Produto Exemplo',
+        description: 'Descrição do produto aqui',
+        price: 100,
+        id: 1,
+        brand: 'apple',
+        discountPercentage: 10,
+        rating: 0,
+        stock: 0,
+        category: '',
+        imagests: '',
+        images: []
+
+    };
+
+    it('renders correctly with data', () => {
+        const actionStub = cy.stub().as('cardAction');
+        mount(<Card data={itemData} action={actionStub} />);
+
+        cy.get('img').should('have.attr', 'src', itemData.thumbnail);
+
+        cy.contains('h4', itemData.title).should('be.visible');
+        cy.contains('p', itemData.description).should('be.visible');
+        cy.contains('p', `R$ ${itemData.price}`).should('be.visible');
+
+        cy.contains('button', 'Comprar').should('be.visible').click();
+        cy.get('@cardAction').should('have.been.calledOnce');
+    });
+});
